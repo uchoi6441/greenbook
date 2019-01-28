d38ac7fcb78154f0152dffb543a41d6ae71f21c7
Urie Choi <urie@Uries-MacBook-Pro.local>
2019-01-27 22:59:41 -0500

    started template header

--- greenbook/App.js
+++ greenbook/App.js
@@ -4,18 +4,43 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>YEET!</Text>
+      <View style={{flex: 1, backgroundColor: "blue"}}>
+        <View style={styles.headingBufferTop}>
+        </View>
+        <View style={styles.heading}>
+          <Text style={styles.headingText}>YEET!</Text>
+        </View>
+        <View style={styles.headingBufferBottom}>
+          <Text style={{fontSize: 20}}>subtitle here</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  heading: {
+    height: '12%',
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headingBufferTop: {
+    height: '8%',
+    width: '100%',
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headingBufferBottom: {
+    height: '6%',
+    width: '100%',
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headingText: {
+    fontSize: 44,
   },
 });
