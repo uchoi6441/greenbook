15a3ac06bf947f4c4dc6ffdb04e913fff2cd83de
Jenny Engelman <jenny.l.engelman.21@dartmouth.edu>
2019-02-04 15:44:06 -0500

    added button feature

--- greenbook/App.js
+++ greenbook/App.js
@@ -3,8 +3,10 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { Font } from 'expo';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { HomeScreen } from './screens/Home';
+import { LogInScreen } from './screens/LogIn';
 
 const RootStack = createStackNavigator({
+  logIn: { screen: LogInScreen },
   Home: { screen: HomeScreen },
 });
 
--- greenbook/components/logInButton.js
+++ greenbook/components/logInButton.js
@@ -2,14 +2,38 @@ import React from 'react';
 import { StyleSheet, Text, Dimensions, TouchableOpacity } from 'react-native';
 import { Font } from 'expo';
 
-export default class PongButton extends React.Component {
+export default class LogInButton extends React.Component {
   state = { fontLoaded: true };
   render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          navigate(this.props.destination)
+        }}
+        style={ styles.button }
+      >
+        <Text style = { this.props.font ? styles.buttonText : styles.buttonTextElse }>
+          { this.props.text }
+        </Text>
+      </TouchableOpacity>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  base: {
-
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    fontFamily: 'gloria-hallelujah',
+    fontSize: 44,
+    color: '#024C2E',
+  },
+  buttonTextElse: {
+    fontSize: 44,
+    color: '#024C2E',
   },
 });
