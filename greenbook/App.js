e1660a0d1762ec4732703bfa62311e0d5977db63
Jenny Engelman <jenny.l.engelman.21@dartmouth.edu>
2019-02-05 22:11:43 -0500

    layout

--- greenbook/App.js
+++ greenbook/App.js
@@ -6,8 +6,10 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { LogInScreen } from './screens/LogIn';
 import { HomeScreen } from './screens/Home';
 import { SignUpScreen } from './screens/SignUp';
+import { CongratsScreen } from './screens/Congrats';
 
 const RootStack = createStackNavigator({
+  Congrats: { screen: CongratsScreen },
   LogIn: { screen: LogInScreen },
   Home: { screen: HomeScreen },
   SignUp: { screen: SignUpScreen },
