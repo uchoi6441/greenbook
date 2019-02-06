c84fb7b2947df966560c2de50c49ad6c5c26aa18
Jenny Engelman <jenny.l.engelman.21@dartmouth.edu>
2019-02-05 21:29:59 -0500

    finished layout

--- greenbook/screens/SignUp.js
+++ greenbook/screens/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, Image, TextInput } from 'react-native';
 import { Font } from 'expo';
 import { StackNavigator } from 'react-navigation';
 import HomeOptions from './../components/HomeOptions';
@@ -39,27 +39,21 @@ export class SignUpScreen extends React.Component {
           />
         </View>
         <View style={ styles.body }>
-          <View style={{flexDirection:'row'}}>
-            <HomeOptions
-              font = { this.state.fontLoaded }
-              text = { 'find' }
-              navigation = { this.props.navigation }
-              destination = { 'Home' }
-            />
+          <View style={ styles.enterBox }>
             <Text style={this.state.fontLoaded ? styles.enter : styles.justInCase }>name:</Text>
             <TextInput
               style={ this.state.fontLoaded ? styles.enter : styles.justInCase }
               onChangeText={(name) => this.setState({name})}
             />
           </View>
-          <View style={{flexDirection:'row'}}>
+          <View style={ styles.enterBox }>
             <Text style={this.state.fontLoaded ? styles.enter : styles.justInCase }>username:</Text>
             <TextInput
               style={ this.state.fontLoaded ? styles.enter : styles.justInCase }
               onChangeText={(username) => this.setState({username})}
             />
           </View>
-          <View style={{flexDirection:'row'}}>
+          <View style={ styles.enterBox }>
             <Text style={this.state.fontLoaded ? styles.enter : styles.justInCase }>password:</Text>
             <TextInput
               style={ this.state.fontLoaded ? styles.enter : styles.justInCase }
@@ -71,12 +65,12 @@ export class SignUpScreen extends React.Component {
           <View style={ styles.bottomImages }>
             <Image
               source={require('./../assets/images/willow.png')}
-              style={ styles.settingsImage }
+              style={ styles.willowImage }
             />
           </View>
-          <View style={{flexDirection: 'column'}}>
-            <Text>continue</Text>
-            <Text>back</Text>
+          <View style={{flexDirection: 'column', alignItems: 'flex-end', marginTop: '10%' }}>
+            <Text style={ this.state.fontLoaded ? styles.continue : styles.else }>continue</Text>
+            <Text style={ this.state.fontLoaded? styles.back : styles.else }>back</Text>
           </View>
         </View>
       </View>
@@ -85,16 +79,35 @@ export class SignUpScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  back: {
+    fontSize: 25,
+    fontFamily: 'gloria-hallelujah',
+    color: '#024C2E',
+  },
   body: {
-    justifyContent: 'center',
     flexDirection: 'column',
-    alignContent: 'space-between',
     marginTop: '30%',
   },
   bottomImages: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-around',
+  },
+  continue: {
+    fontSize: 35,
+    fontFamily: 'gloria-hallelujah',
+    color: '#024C2E',
+  },
+  enterBox: {
+    paddingLeft: 10,
+    alignSelf: 'center',
+    borderRadius: 10,
+    backgroundColor: '#E4E4E4',
+    borderWidth: 2,
+    width: Dimensions.get('window').height / 10 * 4,
+    marginBottom: 30,
+    height: 50,
+    flexDirection: 'row',
   },
   enter: {
     fontSize: 30,
@@ -122,15 +135,11 @@ const styles = StyleSheet.create({
   justInCase: {
     fontSize: 44,
   },
-  cottageImage: {
-    width: 100,
-    height: 100,
-    marginRight: 40,
-  },
-  settingsImage: {
-    width: 60,
-    height: 60,
-    marginLeft: 40,
+  willowImage: {
+    width: 150,
+    height: 150,
+    marginTop: '30%',
+    marginLeft: '10%',
   },
   vineImage: {
     transform: [{ rotate: '180deg'}],
