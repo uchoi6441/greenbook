9100e29b51cb6a477b76f5d193ec33db2680b774
uchoi6441 <35080820+uchoi6441@users.noreply.github.com>
2019-03-06 18:06:37 -0500

    Update MyPostings.js

--- greenbook/screens/MyPostings.js
+++ greenbook/screens/MyPostings.js
@@ -74,7 +74,7 @@ export class MyPostingsScreen extends React.Component {
         <View style={ styles.bottomButtons }>
           <TouchableOpacity
             onPress={() => {
-              navigate({ routeName: 'MakePosting', key: Math.random() * 10000 })
+              navigate("MakePosting")
             }}
           >
             <Text style={ this.state.fontLoaded ? styles.bottomButtonsText : styles.else }>new posting</Text>
